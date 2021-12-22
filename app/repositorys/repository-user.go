1b868823203607cd718f79404aac0127c8a7f750
Kholil <kholil.dsi@dwidasa.com>
2021-12-22 16:52:45 +0700

    repository user

--- app/repositorys/repository-user.go
+++ app/repositorys/repository-user.go
@@ -1,1 +1,18 @@
 package repositorys
+
+import "github.com/jackc/pgx/v4"
+
+type Repositoryuser interface {
+	Create()
+	Update()
+	FindById()
+	FindByEmail()
+}
+
+type repositoryuser struct {
+	db *pgx.Conn
+}
+
+func NewRepositoryuser(db *pgx.Conn) *repositoryuser {
+	return &repositoryuser{db}
+}
